aa19798fb9bd1d71a7e38b5980133c8c1b661f28
Ryan Gee <ryan.gee@btinternet.com>
2019-03-07 00:00:29 +0000

    add jokes/:joke_id endpoint

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const createJoke = require('./jokes/create-joke');
 const getJokes = require('./jokes/get-jokes');
+const getJoke = require('./jokes/get-joke-by-id');
 
 //test response
 router.get('/', function(req, res){
@@ -12,6 +13,8 @@ router.get('/', function(req, res){
 router.route('/create')
 	.post(createJoke.create);
 router.route('/list')
-	.get(getJokes.list)
+	.get(getJokes.list);
+router.route('/:joke_id')
+	.get(getJoke.view);
 
 module.exports = router;
